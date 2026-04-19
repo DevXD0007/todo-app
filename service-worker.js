fe134740dcb478881489077fdca2e26ff1c60a75
DevXD0007 <devendra27209@gmail.com>
2026-04-19 18:44:58 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,18 @@
-const CACHE_NAME="todo-app-v1";
-const urls=["index.html"];
+const CACHE_NAME = "todo-pro-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
 
-self.addEventListener("install",e=>{
-e.waitUntil(
-caches.open(CACHE_NAME).then(cache=>cache.addAll(urls))
-);
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener("fetch",e=>{
-e.respondWith(
-caches.match(e.request).then(res=>res||fetch(e.request))
-);
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
 });
